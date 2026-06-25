6de586ea56d637e2dd7ddee9856e82ab6fc0ee18
will-marella <william_marella@alumni.brown.edu>
2026-06-25 13:48:32 -0400

    Plotting examples

--- Examples/run_DNAm_example.R
+++ Examples/run_DNAm_example.R
@@ -2,10 +2,13 @@ old_wd <- setwd("..")
 on.exit(setwd(old_wd), add = TRUE)
 
 source("main.R")
+source("plotting_helpers.R")
 
 pheno <- readRDS("Examples/ExampleData/pheno_example.rds")
 omics <- readRDS("Examples/ExampleData/dnam_mvalues.rds")
 dir.create("Examples/ExampleResults", showWarnings = FALSE)
+figures_dir <- "Examples/ExampleFigures/DNAm"
+dir.create(figures_dir, recursive = TRUE, showWarnings = FALSE)
 
 additional_covariates <- c("agebl", "agevis", "mbmi")
 
@@ -27,9 +30,13 @@ reports <- FAST_omics_WAS_reports(
 saveRDS(results, "Examples/ExampleResults/dnam_results.rds")
 saveRDS(reports, "Examples/ExampleResults/dnam_reports.rds")
 
+generate_all_plots(results$analysis_change, figures_dir = file.path(figures_dir, "analysis_change"))
+generate_all_plots(results$analysis_level, figures_dir = file.path(figures_dir, "analysis_level"))
+
 cat("DNAm example complete\n")
 cat("  Pheno samples: ", nrow(pheno), "\n", sep = "")
 cat("  DNAm analytes: ", nrow(omics), "\n", sep = "")
 cat("  Change coefficients: ", nrow(results$analysis_change$all$coefficients), "\n", sep = "")
 cat("  Level coefficients: ", nrow(results$analysis_level$all$coefficients), "\n", sep = "")
 cat("  Report summary rows: ", nrow(reports$pheno_summary), "\n", sep = "")
+cat("  Figures: ", figures_dir, "\n", sep = "")
